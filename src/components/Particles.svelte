afb634621ffb6f87ac22f09f53d4d2f51e11af1a
Les Lakewood <les.lakewood@gmail.com>
2025-04-21 17:50:52 -0400

    code: installing Particles

---
+++ src/components/Particles.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
+  let ParticlesComponent;
+
+  onMount(async () => {
+      const module = await import("svelte-particles");
+
+      ParticlesComponent = module.default;
+  });
+
+  let particlesUrl = "../data/Particles.json"; 
+
+  let onParticlesLoaded = event => {
+      const particlesContainer = event.detail.particles;
+  };
+
+  let particlesInit = async engine => {
+      await loadSlim(engine);
+  };
+</script>
+
+<svelte:component
+  this="{ParticlesComponent}"
+  id="tsparticles"
+  class="foo bar"
+  style=""
+  url="{particlesUrl}"
+  on:particlesLoaded="{onParticlesLoaded}"
+  particlesInit="{particlesInit}"
+/>
